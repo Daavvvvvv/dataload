e24da9648f027064ccaa24c312f6b1486d4e5a25
David Franco <83558519+Daavvvvvv@users.noreply.github.com>
2024-07-31 22:34:35 -0500

    Cambios

--- ILoadStrategy.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using System.Data;
-using System.IO;
-using System.Threading.Tasks;
-
-public interface ILoadStrategy
-{
-    Task<List<DataTable>> LoadFiles(List<FileInfo> csvFiles, CsvLoader loader);
-}
