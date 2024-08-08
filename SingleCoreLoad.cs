1789f10019fa3e26261f2cb5e87c86602ff79d25
Daavvvvvv <davidvelez006@gmail.com>
2024-08-07 23:05:25 -0500

    Cambios en SingleCore

--- SingleCoreLoad.cs
+++ SingleCoreLoad.cs
@@ -4,30 +4,41 @@ using System.Data;
 using System.IO;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 public class SingleCoreLoad : ILoad
 {
+    [DllImport("kernel32.dll")]
+    static extern IntPtr GetCurrentThread();
+
+    [DllImport("kernel32.dll")]
+    static extern bool SetThreadAffinityMask(IntPtr hThread, IntPtr dwThreadAffinityMask);
+
     public async Task<List<DataTable>> LoadFiles(List<FileInfo> csvFiles, CsvLoader loader)
     {
         var dataTables = new List<DataTable>();
-        var tasks = new List<Task>();
+        var originalAffinity = Process.GetCurrentProcess().ProcessorAffinity;
 
-        foreach (var file in csvFiles)
+        // Set the current thread to use only one core (Core 0)
+        SetThreadAffinityMask(GetCurrentThread(), new IntPtr(1));
+
+        try
         {
-            tasks.Add(Task.Run(async () =>
+            foreach (var file in csvFiles)
             {
                 var loadStart = Stopwatch.StartNew();
-                var dataTable = loader.LoadCsv(file);
-                lock (dataTables)
-                {
-                    dataTables.Add(dataTable);
-                }
+                var dataTable = loader.LoadCsv(file); // Execute synchronously
+                dataTables.Add(dataTable);
                 loadStart.Stop();
                 Console.WriteLine($"Archivo {file.Name} cargado en {loadStart.ElapsedMilliseconds} ms");
-            }));
+            }
+        }
+        finally
+        {
+            // Restore the original processor affinity
+            Process.GetCurrentProcess().ProcessorAffinity = originalAffinity;
         }
 
-        await Task.WhenAll(tasks);
         return dataTables;
     }
 }
